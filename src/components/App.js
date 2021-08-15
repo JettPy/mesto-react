4448d8b8ed3d62e228dad9c6fc74e5714089c1c7
Sergey Melnikov <55996004+JettPy@users.noreply.github.com>
2021-08-15 22:23:06 +0300

    Костыль 2

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { api } from '../utils/api.js';
+import { api } from '../utils/Api.js';
 import Header from './Header';
 import Main from './Main';
 import Footer from './Footer';
